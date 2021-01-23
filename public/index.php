6dd86af2070b00cbd5daa6de111eb42f06ea6247
Habacuc Emile <habacuc.reginald@gmail.com>
2021-01-23 22:07:32 +0100

    Ajout de l'AltoRouter

--- public/index.php
+++ public/index.php
@@ -3,7 +3,25 @@
  * rôles :
  *    - autoload
  *    - rooter -> c'est en fonction de la page demander charger le bon controller. 
+ *    --https://www.youtube.com/watch?v=tbYa0rJQyoM -> à 15minutes à peut près
  */
 require_once '../vendor/autoload.php';
-require_once '../Controllers/home.php';
-//dump($_SERVER);
+
+$rooter = new AltoRouter();
+$rooter->map('GET','/',function(){
+    require_once '../Controllers/home.php';
+},'home');
+/*
+$rooter->map('GET','/shop','shop');
+$rooter->map('GET','/admin','admin');
+$rooter->map('GET','/member','member');
+$rooter->map('GET','/connexion','connexion');
+$rooter->map('GET','/register','register');
+$rooter->map('GET','/forget','forget');
+$rooter->map('GET','/panier','basket');
+$rooter->map('GET|POST','[*: page]','404');
+//$rooter->map('GET','/legal','');
+*/
+$match = $rooter->match();
+$match['target']();
+//dump($match);
